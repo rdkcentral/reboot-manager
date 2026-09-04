74cc44b6047f302838a56493b8fd8b945e19d484
Saranya <saranya_elango@comcast.com>
2026-09-04 11:57:27 +0000

    Address the review comments

--- reboot-reason-fetcher/src/rebootreason_main.c
+++ reboot-reason-fetcher/src/rebootreason_main.c
@@ -254,10 +254,11 @@ int main(void)
         RDK_LOG(RDK_LOG_INFO,"LOG.RDK.REBOOTINFO","New %s file found, Creating previous reboot info file...\n",REBOOT_INFO_FILE);
         log_reason(PREVIOUS_REBOOT_INFO_FILE);
         has_reboot_info = true;
-	if (access(PARODUS_REBOOT_INFO_FILE, F_OK) == 0) {
-            RDK_LOG(RDK_LOG_INFO,"LOG.RDK.REBOOTINFO","New %s file found, updating parodus logfile...\n", PARODUS_REBOOT_INFO_FILE);
-            handle_parodus_reboot_file(&rebootInfo, PREVIOUS_PARODUSREBOOT_INFO_FILE);
-	}
+	if (handle_parodus_reboot_file(&rebootInfo, PREVIOUS_PARODUSREBOOT_INFO_FILE) != SUCCESS) {
+            RDK_LOG(RDK_LOG_ERROR,"LOG.RDK.REBOOTINFO","Failed to update previous Parodus reboot information\n");
+            ret = ERROR_GENERAL;
+            goto cleanup;
+        }
     }
     else if (errno == ENOENT) {
         RDK_LOG(RDK_LOG_INFO,"LOG.RDK.REBOOTINFO","Deriving reboot reason from legacy sources \n");
@@ -287,6 +288,8 @@ int main(void)
     }
     else {
         RDK_LOG(RDK_LOG_DEBUG,"LOG.RDK.REBOOTINFO","Failed to rename reboot.info: %s\n", strerror(errno));
+        ret = ERROR_GENERAL;
+        goto cleanup;
     }
     // Updating messages.txt
     update_kernel_log(&ctx, &rebootInfo);
